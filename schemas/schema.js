e8ecf16c1455ddd1357162c15d2aa011b1b60d6c
Sylvio <sylvio.migliorucci@gmail.com>
2019-10-22 22:09:52 -0300

    Relationships com user post e hobby

--- schemas/schema.js
+++ schemas/schema.js
@@ -11,21 +11,21 @@ const {
 
 //data sources
 var usersData = [
-    {id: '324',age: 24, name: 'sylvio', job: 'Programmer'}, 
+    {id: '1', age: 24, name: 'sylvio', job: 'Programmer', }, 
     {id: '325',age: 26, name: 'joao',job: 'Artist' }, 
     {id: '326',age: 21, name: 'maria',job: 'actor'},
 ]
 
 var hobbyData = [
-    {id: '1', title: 'Programming', description: 'Using computers for world better place'},
-    {id: '2', title: 'Hiking', description: 'Its a health activity'},
-    {id: '3', title: 'Swimming', description: 'Great on summers'},
+    {id: '1', title: 'Programming', description: 'Using computers for world better place', userId: '1'},
+    {id: '2', title: 'Hiking', description: 'Its a health activity', userId: '1'},
+    {id: '3', title: 'Swimming', description: 'Great on summers', userId: '1'},
 ]
 
 var postData = [
-    {id: '1', comment: 'Building Code'},
-    {id: '2', comment: 'Climbing a mountain'},
-    {id: '3', comment: 'Swimming in the ocean'}
+    {id: '1', comment: 'Building Code', userId: '1'},
+    {id: '2', comment: 'Climbing a mountain', userId: '1'},
+    {id: '3', comment: 'Swimming in the ocean', userId: '1'}
 ]
 
 //Create types
@@ -36,7 +36,7 @@ const UserType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name: { type: GraphQLString},
         age: {type: GraphQLInt},
-        job: { type: GraphQLString}
+        job: { type: GraphQLString},
     })
 })
 
@@ -47,7 +47,12 @@ const HobbyType = new GraphQLObjectType({
         id: {type: GraphQLID},
         title: {type: GraphQLString},
         description: {type: GraphQLString},
-
+        user: { 
+            type: UserType,
+            resolve(parent, args) {
+                return _.find(usersData, {id: parent.userId})
+            }
+        }
     })
 })
 
@@ -56,7 +61,13 @@ const PostType = new GraphQLObjectType({
     description: 'A Post for users see my hobbies ',
     fields: () => ({
         id: {type: GraphQLID},
-        comment: {type: GraphQLString}
+        comment: {type: GraphQLString},
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                return _.find(usersData, {id: parent.userId})
+            }
+        }
     })
 })
 
